6b0c95c29c5a2e80455ae65f25e45418327ef4ec
Cassim Ahmed Attia <cassimahmedattia@gmail.com>
2021-07-15 10:11:02 +0500

    test

--- testing.r
+++ testing.r
@@ -2,4 +2,4 @@ name = "Cassim"
 version = "Rstudio - In writing"
 
 print(name)
-print("These lines has been written in Rstudio is good as wll.")
+print("These lines has been written in Rstudio is good as well.")
